d2acda6be5eb00ca770ab6db56ea854ec3ca5afa
DimSkakovskyi <dimonskak@ukr.net>
2025-09-22 21:38:47 +0300

    7-state-via-props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import Random from './components/Random.tsx';
+import Counter from './components/Counter';
+import Button from './components/Button';
+import { useState } from 'react';
 
 const App = () => {
-  return <div><Random min={40} max={70} /></div>
-};
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => setCount(count + 1);
+
+  const decrementCount = () => setCount(count - 1);
+
+  const resetCount = () => setCount(0);
+
+  return <div>
+    <Counter count={count}/>
+    <Button onClick={incrementCount} caption='Increment'/>
+    <Button onClick={decrementCount} caption='Decrement'/>
+    <Button onClick={resetCount} caption='Reset'/>
+  </div>;
+}
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+interface ButtonProps {
+  caption: string,
+  onClick: () => void;
+}
+
+const Button = ({ caption, onClick }: ButtonProps) => {
+  return (
+    <button onClick={onClick}>{caption}</button>
+  );
+};
+
+export default Button;
