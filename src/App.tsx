1322d4991d1b28863986ef001fba28a8b1333f42
DimSkakovskyi <dimonskak@ukr.net>
2025-09-22 21:11:31 +0300

    6-react-typescript

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Random from './components/Random.tsx';
+
+const App = () => {
+  return <div><Random min={40} max={70} /></div>
+};
+
+export default App;
---
+++ src/utils/randomizer.ts
@@ -0,0 +1,5 @@
+const randomizer = (from: number = 0, to: number = 100) => {
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+};
+
+export default randomizer;
