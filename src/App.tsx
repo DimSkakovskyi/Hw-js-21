432583d02e0c4d232761351243b22efec7ed7a1c
DimSkakovskyi <dimonskak@ukr.net>
2025-09-22 21:48:42 +0300

    8-map

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import Counter from './components/Counter';
-import Button from './components/Button';
-import { useState } from 'react';
+import Counter from "./components/Counter";
+import Button from "./components/Button";
+import { useState } from "react";
+
+const captionArray = ["Increment", "Count + 1", "Increase", "One More"];
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -11,12 +13,18 @@ const App = () => {
 
   const resetCount = () => setCount(0);
 
-  return <div>
-    <Counter count={count}/>
-    <Button onClick={incrementCount} caption='Increment'/>
-    <Button onClick={decrementCount} caption='Decrement'/>
-    <Button onClick={resetCount} caption='Reset'/>
-  </div>;
-}
+  return (
+    <div>
+      <Counter count={count} />
+
+      {captionArray.map((captionElement, index) => {
+        return <Button onClick={incrementCount} caption={captionElement} key={index}/>;
+      })}
+
+      <Button onClick={decrementCount} caption="Decrement" />
+      <Button onClick={resetCount} caption="Reset" />
+    </div>
+  );
+};
 
 export default App;
